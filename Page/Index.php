930d2befb8afa4ae1a030cc003ebab178ef6145e
Creazzo Nicola <nicola.creazzo.007@gmail.com>
2025-10-22 22:47:05 +0200

    CSS grid Checkbox ingredienti Commenti al CSS per studio futuro Nuove immagini + ritaglio immagini vecchie

--- Page/Index.php
+++ Page/Index.php
@@ -15,7 +15,7 @@
                 <h1 class="logo">La panineria dei programmatori</h1>
                 <div class="nav-links">
                     <button><a href="Index.php">Home</a></button>
-                    <button><a href="Fidelty.php">Servizi</a></button>
+                    <button><a href="Fidelity.php">Servizi</a></button>
                     <button><a href="Output.php">Ordine</a></button>
                 </div>
             </nav>
@@ -38,14 +38,58 @@
                     <label for="pane">Scegli il tipo di pane:</label>
                     <select id="pane" name="pane" oninput="changePicture(this)" required>
                         <option value="" disabled selected>-- Seleziona il tipo di  pane --</option>
-                        <option value="XAMPP">Pane XAMPP</option>   
-                        <option value="XMIND">Pane XMIND</option>
-                        <option value="IntelliJ">Pane IntelliJ</option>
-                        <option value="VsCode">Pane VsCode</option>
-                        <option value="VsCommunity">Pane VsCommunity</option>
+                        <option value="XAMPP-3">Pane XAMPP - 3€</option>   
+                        <option value="XMIND-5">Pane XMIND - 5€</option>
+                        <option value="IntelliJ-4">Pane IntelliJ - 4€</option>
+                        <option value="VsCode-2">Pane VsCode - 2€</option>
+                        <option value="VsCommunity-3">Pane VsCommunity - 3€</option>
                     </select><br><br>
 
-                    <img src="../imgs/logo2.png" alt="Immagine Panini" class="img-panini" id="img-panini"><br><br>
+                    <img src="../imgs/buns.png" alt="Immagine Panini" class="img-panini" id="img-panini"><br><br>
+
+                    <!-- Scelta Ingredienti -->
+                    <!-- Sistema di div annidiati per usare la CSS grid -->
+                    <label>Scegli gli ingredienti:</label><br>
+
+                    <div class="ingrediente">
+                        <div class="info">
+                            <input type="checkbox" id="ingrediente1" name="ingrediente[]" value="Lettuce-0.3">
+                            <label for="ingrediente1">Lattuga (+0.30€)</label>
+                        </div>
+                        <img src="../imgs/lettuce.png" alt="Lattuga" class="icon-ingrediente">
+                    </div>
+
+                    <div class="ingrediente">
+                        <div class="info">
+                            <input type="checkbox" id="ingrediente2" name="ingrediente[]" value="Tomato-0.3">
+                            <label for="ingrediente2">Pomodoro (+0.30€)</label>
+                        </div>
+                        <img src="../imgs/tomato.png" alt="Pomodoro" class="icon-ingrediente">
+                    </div>
+
+                    <div class="ingrediente">
+                        <div class="info">
+                            <input type="checkbox" id="ingrediente3" name="ingrediente[]" value="Cheese-0.3">
+                            <label for="ingrediente3">Formaggio (+0.30€)</label>
+                        </div>
+                        <img src="../imgs/cheese.png" alt="Formaggio" class="icon-ingrediente">
+                    </div>
+
+                    <div class="ingrediente">
+                        <div class="info">
+                            <input type="checkbox" id="ingrediente4" name="ingrediente[]" value="Bacon-0.5">
+                            <label for="ingrediente4">Bacon (+0.50€)</label>
+                        </div>
+                        <img src="../imgs/bacon.png" alt="Bacon" class="icon-ingrediente">
+                    </div>
+
+                    <div class="ingrediente">
+                        <div class="info">
+                            <input type="checkbox" id="ingrediente5" name="ingrediente[]" value="Onion-0.8">
+                            <label for="ingrediente5">Onion rings (+0.80€)</label>
+                        </div>
+                        <img src="../imgs/onion.png" alt="Onion rings" class="icon-ingrediente">
+                    </div>
 
                     <!-- Invio e Reset -->
                     <input type="submit" value="Invia">
